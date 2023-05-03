9c561ea448052dc9b79b10f3c09a96b70be6b3e8
youness_ifrah <younessifrah4@gmail.com>
2023-05-03 16:03:18 +0100

    an other commit, commit n° 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //modification du code
+        System.out.println("This is a first modification of this java code");
+
     }
 }
